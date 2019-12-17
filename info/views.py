cc768d30ee22ca3610ff86b566946607aca45fbc
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-12-17 17:54:25 +0300

    init index page

--- info/views.py
+++ info/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+class Index(View):
+    template = 'info/index.html'
+    
+    def get(self, request):
+        context = {}
+        return render(request, self.template, context=context)
