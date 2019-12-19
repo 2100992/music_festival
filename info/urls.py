25bb899d92d26a0c937648ccb1b819028fc14791
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-12-19 18:44:45 +0300

    new path

--- info/urls.py
+++ info/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import Index, Participans, Locations
-from .views import Gallery, Blog, Infrastructure, Contacts
+from .views import Index, Participans, Locations, Blog
+from .views import Gallery, Infrastructure, Contacts, Road
 
 urlpatterns = [
     path('', Index.as_view(), name='index_url'),
@@ -8,6 +8,7 @@ urlpatterns = [
     path('locations/', Locations.as_view(), name='locations_url'),
     path('gallery/', Gallery.as_view(), name='gallery_url'),
     path('blog/', Blog.as_view(), name='blog_url'),
+    path('road/', Road.as_view(), name='road_url'),
     path('infrastructure/', Infrastructure.as_view(), name='infrastructure_url'),
     path('contacts/', Contacts.as_view(), name='contacts_url'),
 ]
