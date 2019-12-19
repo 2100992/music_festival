16ce4c232449de528a49fe53190d027bdc6547fd
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-12-19 18:44:27 +0300

    some experiments

--- info/views.py
+++ info/views.py
@@ -1,19 +1,34 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
+
 from info.models import Info, Participant, Location
 from django.shortcuts import get_object_or_404, get_list_or_404
+
+
 
 # Create your views here.
 
 
-class Index(View):
-    template = 'info/index.html'
-    model_info = Info
+# class Index(View):
+#     template = 'info/index.html'
+#     model_info = Info
     
-    def get(self, request):
+#     def get(self, request):
+#         context = {}
+#         context['info'] = get_object_or_404(self.model_info, destination='index')
+#         return render(request, self.template, context=context)
+
+
+class Index(TemplateView):
+    template_name = 'info/index.html'
+    model_info = Info
+
+    def get_context_data(self, **kwargs):
         context = {}
         context['info'] = get_object_or_404(self.model_info, destination='index')
-        return render(request, self.template, context=context)
+        return context
+
 
 class Participans(View):
     template = 'info/participants.html'
@@ -23,7 +38,7 @@ class Participans(View):
     def get(self, request):
         context = {}
         context['info'] = get_object_or_404(self.model_info, destination='participants')
-        context['participants'] = self.model.objects.all()
+        context['participants'] = Participant.objects.all()
         return render(request, self.template, context=context)
 
 class Locations(View):
@@ -34,7 +49,7 @@ class Locations(View):
     def get(self, request):
         context = {}
         context['info'] = get_object_or_404(self.model_info, destination='locations')
-        context['locations'] = self.model.objects.all()
+        context['locations'] = Location.objects.all()
         return render(request, self.template, context=context)
 
 class Gallery(View):
@@ -45,6 +60,13 @@ class Gallery(View):
         return render(request, self.template, context=context)
 
 class Blog(View):
+    template = 'info/index.html'
+    
+    def get(self, request):
+        context = {}
+        return render(request, self.template, context=context)
+
+class Road(View):
     template = 'info/index.html'
     
     def get(self, request):
@@ -64,3 +86,17 @@ class Contacts(View):
     def get(self, request):
         context = {}
         return render(request, self.template, context=context)
+
+
+# class Login(LoginView):
+#     template_name = 'p_library/login.html'
+
+#     def get_success_url(self):
+#         url = self.get_redirect_url()
+#         if url:
+#             return url
+#         elif self.request.user.is_superuser:
+#             return '/admin/'
+#             # return reverse('admin')   # почему-то не работает
+#         else:
+#             return reverse('p_library:library_url')
