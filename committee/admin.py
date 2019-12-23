cf5d69faebaf223931eb94cd85c78c379f152d84
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-12-23 16:39:49 +0300

    init commit

--- committee/admin.py
+++ committee/admin.py
@@ -1,3 +1,13 @@
+from codecs import register
 from django.contrib import admin
+from committee.models import Applicant, Committeeman
 
 # Register your models here.
+
+@admin.register(Applicant)
+class ApplicantAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Committeeman)
+class CommitteemanAdmin(admin.ModelAdmin):
+    pass
