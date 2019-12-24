711bb3c5abc8b8492ff012f9a206672b83ff70e6
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-12-24 18:50:25 +0300

    added info page on committe

--- committee/views.py
+++ committee/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
+
+from committee.utils import GetContextDataMixin
+
+from committee.models import Info
 # Create your views here.
 
-class Index(TemplateView):
+class Index(GetContextDataMixin, TemplateView):
     template_name = 'committee/index.html'
+    # applicant_template_name = 'committee/applicant.html'
+    # committeeman_template_name = 'committee/committeeman.html'
+    model_info = Info
+    destination = 'index'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().dispatch(request, *args, **kwargs)
+        else:
+            return redirect('/')
+
+        
