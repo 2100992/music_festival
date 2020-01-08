cd3598683638936e59d4d73de7f96ee38357b52e
Shvd <2100992@gmail.com>
2020-01-08 16:38:50 +0300

    added new features

--- blog/admin.py
+++ blog/admin.py
@@ -2,10 +2,25 @@ from codecs import register
 from django.contrib import admin
 
 from django.contrib.auth.models import User
-from blog.models import Post
+from blog.models import Post, Category
 
 # Register your models here.
 
+
+class PostInline(admin.TabularInline):
+    model = Post.category.through
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        PostInline,
+    ]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # inlines = [
+    #     PostInline,
+    # ]
+    exclude = ('post',)
