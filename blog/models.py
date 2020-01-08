dc54f78dd5e25c0a89114da010dabba7662df95b
Shvd <2100992@gmail.com>
2020-01-08 16:39:13 +0300

    added new features

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,17 @@ from markdown import markdown
 from blog.utils import make_unique_slug
 from blog.translater import translate
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
 class Post(models.Model):
+    author = models.ForeignKey(
+        User,
+        null=True,
+        default=None,
+        on_delete=models.CASCADE
+    )
     title = models.CharField(max_length=255)
     slug = models.SlugField(
         default="_",
@@ -46,6 +54,29 @@ class Post(models.Model):
         if self.convetr_md_to_html:
             self.convetr_md_to_html = False
             self.html_field = markdown(self.markdown_field)
+
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(
+        default="_",
+        db_index=True,
+        unique=True
+    )
+    post = models.ManyToManyField(
+        Post,
+        related_name='category',
+    )
+
+    def save(self, *args, **kwargs):
+
+        if not self.id:
+            self.slug = make_unique_slug(self.__class__, translate(self.title))
 
         super().save(*args, **kwargs)
     
