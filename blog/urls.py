8a7c3bb8f1a358e2646420873ef1e53bd1bc46aa
Shvd <2100992@gmail.com>
2020-01-13 09:14:55 +0300

    experiments

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from blog import views
 
 urlpatterns = [
     path('api/posts/', views.PostList.as_view(), name='posts-list'),
+    # path('api/posts/', views.post_list, name='posts-list'),
     path('api/post/<str:slug>/', views.PostDetail.as_view(), name='post-detail'),
     path('api/categories/', views.CategoryList.as_view(), name='categories-list'),
     path('api/category/<str:slug>/', views.CategoryDetail.as_view(), name='category-detail'),
