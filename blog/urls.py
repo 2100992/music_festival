6ee928896411498d62a0628a280230f29ef89d0d
Швецов Сергей Владимирович <shvetsov@elvees.com>
2020-01-24 16:37:10 +0300

    changed to a single view urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('api/posts/', views.PostList.as_view(), name='posts-list'),
     # path('api/posts/', views.post_list, name='posts-list'),
-    path('api/post/<str:slug>/', views.PostDetail.as_view(), name='post-detail'),
+    path('api/posts/<str:slug>/', views.PostDetail.as_view(), name='post-detail'),
     path('api/categories/', views.CategoryList.as_view(), name='categories-list'),
-    path('api/category/<str:slug>/', views.CategoryDetail.as_view(), name='category-detail'),
+    path('api/categories/<str:slug>/', views.CategoryDetail.as_view(), name='category-detail'),
 ]
